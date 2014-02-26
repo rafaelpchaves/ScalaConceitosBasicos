2cf04bedc8c5d2b7ff0b6ef18da18ac88f6e9f4c
rafaelpchaves <rafaelpchaves@gmail.com>
2014-02-25 22:21:03 -0300

    Metodos scala

--- src/Metodos.scala
+++ src/Metodos.scala
@@ -2,16 +2,25 @@
 class Metodos {
 }
 
-  object metodos {
-    class Upper {
-      def upper(strings: String*): Seq[String] = {
-        strings.map((s: String) => s.toUpperCase())
-      }
-    }
-    def main(args: Array[String]) {
-      val up = new Upper
-      Console.println(up.upper("A", "First", "Scala", "Program"))
+//Instância somente esse objeto
 
+object metodos {
+  class Upper {
+    def upper(strings: String*): Seq[String] = {
+      strings.map((s: String) => s.toUpperCase())
     }
+  }
+  def main(args: Array[String]) {
+    val up = new Upper
+    Console.println(up.upper("A", "First", "Scala", "Program"))
 
   }
+  
+  object Upper {
+    def upper(strings: String*) = strings.map(_.toUpperCase())
+  }
+  println(Upper.upper("A", "First", "Scala", "Program"))
+
+}
+  
+  
